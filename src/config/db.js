bfce10ef6dc6ed2e03b15d57afbd98f0a25e9d5e
DESKTOP-5714MCD\91966 <mrrathod0007@gmail.com>
2024-03-15 12:03:59 +0530

    assdjas

--- src/config/db.js
+++ src/config/db.js
@@ -1,15 +1,38 @@
 const mongoose = require("mongoose")
 require('dotenv').config();
 
-// mongoose.connect("mongodb://127.0.0.1:27017/MyShop")
-const conn = await mongoose.connect(process.env.MONGO_URL)
+const username = encodeURIComponent("mrrathod0007");
+const password = encodeURIComponent("Mahi9036@yahoo.com");
+const cluster = "cluster0";
+const authSource = "<authSource>";
+const authMechanism = "<authMechanism>";
+// mongodb+srv://mrrathod0007:Mahi9036@yahoo.com@cluster0.t2hxubb.mongodb.net/
 
-.then(()=>{
-    console.log(`MongoDb Conntected: ${conn.connection.host}`)
-})
-.catch(()=>{
-    console.log("failed to connect")
-})
+let uri =
+  `mongodb+srv://${username}:${password}@${cluster}.t2hxubb.mongodb.net/`;
+const client = new MongoClient(uri);
+
+async function run() {
+    try {
+      await client.connect();
+      const database = client.db("<dbName>");
+      const ratings = database.collection("<collName>");
+      const cursor = ratings.find();
+      await cursor.forEach(doc => console.dir(doc));
+    } finally {
+      await client.close();
+    }
+  }
+  run().catch(console.dir);
+// mongoose.connect("mongodb://127.0.0.1:27017/MyShop")
+// const conn = await mongoose.connect(process.env.MONGO_URL)
+
+// .then(()=>{
+//     console.log(`MongoDb Conntected: ${conn.connection.host}`)
+// })
+// .catch(()=>{
+//     console.log("failed to connect")
+// })
 
 const LoginSchema = new mongoose.Schema({
     mobile:{
